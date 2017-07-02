93ad1b72230cdfcfdeca07a183006f5211da1b59
Debora Csomos <csomidebi@gmail.com>
2017-07-02 19:41:21 +0200

    added main() and one exception handling

--- part2/printing.py
+++ part2/printing.py
@@ -3,11 +3,6 @@
 from reports import *
 
 
-# asking for input from Judy
-data_file = input("Give name of data file (e.g. game_stat.txt): \n")
-title = input("Enter title for question 6 (e.g. Diablo II): \n")
-
-
 # prints the most played game
 def print_get_most_played(file_name):
     answer1 = get_most_played(file_name)
@@ -40,14 +35,17 @@ def print_get_date_avg(file_name):
 
 # prints the properties of the given game
 def print_get_game(file_name, title):
-    answer6 = get_game(file_name, title)
-    for i, item in enumerate(answer6):
-        answer6[i] = str(item)
-    properties = ["name", "copies sold", "release date", "genre", "publisher"]
-    print ("The properties of {} are the following:".format(title))
-    for i, prop in enumerate(properties):
-        print ("{}: {}".format(properties[i], answer6[i]))
-    print ("\n")
+    try:
+        answer6 = get_game(file_name, title)
+        for i, item in enumerate(answer6):
+            answer6[i] = str(item)
+        properties = ["name", "copies sold", "release date", "genre", "publisher"]
+        print ("The properties of {} are the following:".format(title))
+        for i, prop in enumerate(properties):
+            print ("{}: {}".format(properties[i], answer6[i]))
+        print ("\n")
+    except TypeError:
+        print ("Game not found\n")
 
 
 # prints genres and the number of games rated in that genre
@@ -66,11 +64,18 @@ def print_get_date_ordered(file_name):
     print ("\n".join(answer8), end="\n"*2)
 
 
-print_get_most_played(data_file)
-print_sum_sold(data_file)
-print_get_selling_avg(data_file)
-print_count_longest_title(data_file)
-print_get_date_avg(data_file)
-print_get_game(data_file, title)
-print_count_grouped_by_genre(data_file)
-print_get_date_ordered(data_file)
+def main():
+    data_file = input("Give name of data file (e.g. game_stat.txt): \n")
+    title = input("Enter title for question 6 (e.g. Diablo II): \n")
+    print_get_most_played(data_file)
+    print_sum_sold(data_file)
+    print_get_selling_avg(data_file)
+    print_count_longest_title(data_file)
+    print_get_date_avg(data_file)
+    print_get_game(data_file, title)
+    print_count_grouped_by_genre(data_file)
+    print_get_date_ordered(data_file)
+
+
+if __name__ == "__main__":
+    main()
